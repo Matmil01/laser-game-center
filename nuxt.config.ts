f342caebb7951a76fa238fe13570fc8d5ccb8736
Matmil01 <matmil01@easv365.dk>
2026-03-25 12:45:20 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,5 +6,16 @@ export default defineNuxtConfig({
   css: ['~/assets/main.css'],
   vite: {
     plugins: [tailwindcss()]
+  },
+  app: {
+    head: {
+      title: 'Laser Game Center',
+      meta: [
+        { name: 'description', content: 'Velkommen til Laser Game Center!' }
+      ],
+      // link: [
+      //   { rel: 'icon', type: 'image/png', href: '/favicon.png' }
+      // ]
+    }
   }
 })
