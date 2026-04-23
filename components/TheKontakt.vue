3cc40939351b4ffccb5a10ee12737e9be6215303
Emisim01 <emisim01@easv365.dk>
2026-04-23 10:22:42 +0200

    decent theKontakt - mangler stadig noget

--- components/TheKontakt.vue
+++ components/TheKontakt.vue
@@ -1,5 +1,23 @@
 <template>
-
+  <div class="flex flex-col md:flex-row gap-8 max-w-4xl mx-auto my-10">
+    <!-- Kontakt os -->
+    <div class="flex-1 p-6 border-neon-neonred">
+      <h2 class="text-white text-xl font-bold mb-4">Kontakt os</h2>
+        <p class="mb-2 text-white"><span class="font-semibold">Adresse:</span> Mønstervej 60 6854 Henne</p>
+        <p class="mb-2 text-white"><span class="font-semibold">Telefon:</span> <a href="tel:+4512345678" class=" hover:underline">+45 29 46 56 75 </a></p>
+        <p class="text-white"><span class="font-semibold">Email:</span> <a href="mailto:lasergamecenter@gmail.com" class=" hover:underline">lasergamecenter@gmail.com</a></p>
+    </div>
+    <!-- Maps -->
+    <div class="flex-1 flex items-center justify-center border-neon-neonred">
+      <iframe
+        src="https://www.google.com/maps?q=Mønstervej+60,+6854+Henne&output=embed"
+        class="w-full h-64 border-0"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade">
+      </iframe>
+    </div>
+  </div>
 </template>
 
 <script>
