a3d5a83806ffa33492346926bfd9448e6f6b12f4
Luna Damm Andersen <luna@Lunas-MacBook-Pro-2.local>
2026-04-23 09:34:02 +0200

    added fucking awesome components som bare virker

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,10 @@
 <template>
   <h1 class="text-3xl font-bold">This is the "/" page"</h1>
+  <TheHero />
+  <TheVelkomst />
+  <TheFotokarrusel />
+  <ThePriser />
+  <TheReglerForSpillet />
+  <TheArrangementer />
+  <TheKontakt />
 </template>
