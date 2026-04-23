72251f0dd09e9f612cd8735993ea3e751404dd31
Luna Damm Andersen <luna@Lunas-MacBook-Pro-2.local>
2026-04-23 10:04:26 +0200

    added bg og 1200px max

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,16 @@
 <template>
-  <h1 class="text-3xl font-bold text-neonred">This is the "/" page"</h1>
-  <TheHero />
-  <TheVelkomst />
-  <TheFotokarrusel />
-  <ThePriser />
-  <TheReglerForSpillet />
-  <TheArrangementer />
-  <TheKontakt />
+<div class="bg-black min-h-screen flex justify-center">
+    <div class="w-[1200px] mx-auto">
+        <h1 class="text-3xl font-bold text-neonred">This is the "/" page"</h1>
+        <TheHero />
+        <TheVelkomst />
+        <TheFotokarrusel />
+        <ThePriser />
+        <TheReglerForSpillet />
+        <TheArrangementer />
+        <TheKontakt />
+    </div>
+</div>
+
+
 </template>
