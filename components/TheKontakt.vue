ee66519c2eb67a98ef942d6f067328cba2ee3cd0
Emisim01 <emisim01@easv365.dk>
2026-04-23 10:27:32 +0200

    bedre størrelse på kasser

--- components/TheKontakt.vue
+++ components/TheKontakt.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col md:flex-row gap-8 max-w-4xl mx-auto my-10">
+  <div class="flex flex-col md:flex-row gap-8 p-8">
     <!-- Kontakt os -->
     <div class="flex-1 p-6 border-neon-neonred">
       <h2 class="text-white text-xl font-bold mb-4">Kontakt os</h2>
@@ -8,7 +8,7 @@
         <p class="text-white"><span class="font-semibold">Email:</span> <a href="mailto:lasergamecenter@gmail.com" class=" hover:underline">lasergamecenter@gmail.com</a></p>
     </div>
     <!-- Maps -->
-    <div class="flex-1 flex items-center justify-center border-neon-neonred">
+    <div class="flex-1 border-neon-neonred p-6 flex items-center justify-center">
       <iframe
         src="https://www.google.com/maps?q=Mønstervej+60,+6854+Henne&output=embed"
         class="w-full h-64 border-0"
