57864605dad4f94905d88626a4b04fe6200374d3
Luna Damm Andersen <luna@Lunas-MacBook-Pro-2.local>
2026-04-23 10:11:51 +0200

    neon borders

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="bg-black min-h-screen flex justify-center">
     <div class="w-[1200px] mx-auto">
-        <h1 class="text-3xl font-bold text-neonred">This is the "/" page"</h1>
+        <h1 class="text-3xl font-bold text-neonred border-neon-neonred">This is the "/" page"</h1>
         <TheHero />
         <TheVelkomst />
         <TheFotokarrusel />
