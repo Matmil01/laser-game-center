c4d0fe863be089e317ac6a3e48c53dff295af00d
Matmil01 <matmil01@easv365.dk>
2026-04-25 19:54:42 +0200

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="bg-black">
   <nav class="px-6 py-4 w-300 mx-auto">
-      <h1 class="text-white text-2xl font-bold">JEG FUCKING ELSKER LASERTAG</h1>
+      <h1 class="text-white text-2xl font-bold">LASER GAME CENTER</h1>
   </nav>
 </div>
 </template>
