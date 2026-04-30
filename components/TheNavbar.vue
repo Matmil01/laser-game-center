34c0c1d7a78da4ec9008c3809a84e0f9faf7e043
Emisim01 <emisim01@easv365.dk>
2026-04-30 10:04:59 +0200

    logo is here

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,7 +1,9 @@
 <template>
 <div class="bg-black sticky top-0 z-50">
   <nav class="px-6 py-4 w-300 mx-auto flex items-center justify-between">
-    <h1><NuxtLink to="/" class="text-white text-2xl font-bold">LASER GAME CENTER</NuxtLink></h1>
+<h1 class="flex items-center gap-2">
+  <NuxtLink to="/"><img src="/icons/LogoHvid.svg" alt="Logo" class="h-15 inline-block" /></NuxtLink>
+</h1> 
     <div class="flex items-center gap-6">
       <a href="#priser" class="text-white text-sm hover:text-neonred transition-colors duration-300">Priser</a>
       <a href="#regler" class="text-white text-sm hover:text-neonred transition-colors duration-300">Regler</a>
