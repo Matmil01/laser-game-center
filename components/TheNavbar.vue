aff85841eee5bf87ebab6ddb565a7dbf4cba4bf2
Mirkel1998 <mikped01@easv365.dk>
2026-04-30 09:57:22 +0200

    Navbar
    Scroll issues fixed

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="bg-black sticky top-0 z-50">
   <nav class="px-6 py-4 w-300 mx-auto flex items-center justify-between">
-    <h1><NuxtLink to="/" class="text-white text-2xl font-bold">LASER GAME CENTER</NuxtLink></h1>
+    <h1><a href="#top" class="text-white text-2xl font-bold">LASER GAME CENTER</a></h1>
     <div class="flex items-center gap-6">
       <a href="#priser" class="text-white text-sm hover:text-neonred transition-colors duration-300">Priser</a>
       <a href="#regler" class="text-white text-sm hover:text-neonred transition-colors duration-300">Regler</a>
