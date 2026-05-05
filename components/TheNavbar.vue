11eb3a41b44eb9f50037ebea0b2df4a6a086b19f
Luna Damm Andersen <luna@lunand.dk>
2026-05-05 10:56:05 +0200

    TheNavBar now goes ned to the korrekte one når man is under the bookingside :P

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -2,14 +2,14 @@
 <div class="bg-black sticky top-0 z-50">
   <nav class="px-6 py-4 max-w-300 mx-auto flex items-center justify-between">
 <h1 class="flex items-center gap-2">
-  <a href="#top"><img src="/icons/logoHvid.svg" alt="Logo" class="h-10 sm:h-15 inline-block" /></a>
+  <a href="/"><img src="/icons/logoHvid.svg" alt="Logo" class="h-10 sm:h-15 inline-block" /></a>
 </h1>
 
     <div class="flex items-center gap-6">
-      <a href="#priser" class="text-white text-sm hover:text-neonred transition-colors duration-300 hidden md:block">Priser</a>
-      <a href="#regler" class="text-white text-sm hover:text-neonred transition-colors duration-300 hidden md:block">Regler</a>
-      <a href="#arrangementer" class="text-white text-sm hover:text-neonred transition-colors duration-300 hidden md:block">Arrangementer</a>
-      <a href="#kontakt" class="text-white text-sm hover:text-neonred transition-colors duration-300 hidden md:block">Kontakt</a>
+      <a href="/#priser" class="text-white text-sm hover:text-neonred transition-colors duration-300 hidden md:block">Priser</a>
+      <a href="/#regler" class="text-white text-sm hover:text-neonred transition-colors duration-300 hidden md:block">Regler</a>
+      <a href="/#arrangementer" class="text-white text-sm hover:text-neonred transition-colors duration-300 hidden md:block">Arrangementer</a>
+      <a href="/#kontakt" class="text-white text-sm hover:text-neonred transition-colors duration-300 hidden md:block">Kontakt</a>
       <NeonButton to="/booking" text="BOOKING" custom-class="!px-4 !py-1 text-sm" />
     </div>
   </nav>
