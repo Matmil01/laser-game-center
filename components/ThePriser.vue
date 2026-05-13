e21678b5161fb7919a60131847b88edaf33083f7
Luna Damm Andersen <luna@lunand.dk>
2026-05-13 12:19:54 +0200

    ændret some small stavemistakes

--- components/ThePriser.vue
+++ components/ThePriser.vue
@@ -4,7 +4,7 @@
   <div class="sm:flex gap-8 p-8 mb-7">
     <!-- Card 1 - Green -->
     <div class="flex-1 border-neon-neongreen p-8 flex flex-col mb-6 md:mb-0">
-      <h2 class="text-2xl text-white text-center mb-6">1. {{ $t('priser.game') }}</h2>
+      <h2 class="text-2xl text-white text-center mb-6">1 {{ $t('priser.game') }}</h2>
       <div class="border-b-2 border-neongreen mb-8" style="box-shadow: 0 0 18px 3px #00FF00;"></div>
 
       <div class="flex-grow">
@@ -17,7 +17,7 @@
 
     <!-- Card 2 - Pink -->
     <div class="flex-1 border-neon-neonpink p-8 flex flex-col mb-6 md:mb-0">
-      <h2 class="text-2xl text-white text-center mb-6">2. {{ $t('priser.game') }}</h2>
+      <h2 class="text-2xl text-white text-center mb-6">2 {{ $t('priser.games') }}</h2>
       <div class="border-b-2 border-neonpink mb-8" style="box-shadow: 0 0 18px 3px #FF009D;"></div>
 
       <div class="flex-grow">
@@ -30,7 +30,7 @@
 
     <!-- Card 3 - Orange -->
     <div class="flex-1 border-neon-neonorange p-8 flex flex-col mb-6 md:mb-0">
-      <h2 class="text-2xl text-white text-center mb-6">3. {{ $t('priser.game') }}</h2>
+      <h2 class="text-2xl text-white text-center mb-6">3 {{ $t('priser.games') }}</h2>
       <div class="border-b-2 border-neonorange mb-8" style="box-shadow: 0 0 18px 3px #FF9D00;"></div>
 
       <div class="flex-grow">
@@ -43,7 +43,7 @@
 
     <!-- Card 4 - Blue -->
     <div class="flex-1 border-neon-neonblue p-8 flex flex-col mb-6 md:mb-0">
-      <h2 class="text-2xl text-white text-center mb-6">4. {{ $t('priser.game') }}</h2>
+      <h2 class="text-2xl text-white text-center mb-6">4 {{ $t('priser.games') }}</h2>
       <div class="border-b-2 border-neonblue mb-8" style="box-shadow: 0 0 18px 3px #00C3FF;"></div>
 
       <div class="flex-grow">
