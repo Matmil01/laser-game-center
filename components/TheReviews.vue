3a489209ce99d74b2a3a0e2e7af5c99f0d07ef1c
Luna Damm Andersen <luna@lunand.dk>
2026-05-18 18:51:24 +0200

    kommenterede stjernene ud fordi de var honestly lidt for meget idkkkkkkk

--- components/TheReviews.vue
+++ components/TheReviews.vue
@@ -57,13 +57,13 @@
                             <h4 class="text-white font-light text-xl">
                                 {{ review.name }}
                             </h4>
-                            <div class="flex flex-col">
+                            <!-- <div class="flex flex-col">
                                 <p
                                     class="text-neonorange md:text-4xl text-3xl md:-mt-3"
                                 >
                                     ★★★★★
                                 </p>
-                            </div>
+                            </div> -->
                         </div>
                         <p class="text-white font-light text-content">
                             {{ review.comment }}
